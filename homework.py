7fde942f644376259be768a86a9dc7966ded5959
Badmantegrow1 <Badmantegrow1@gmail.com>
2022-12-17 04:13:54 +0300

    work on bugs 9

--- homework.py
+++ homework.py
@@ -90,8 +90,8 @@ def check_response(response):
     """Проверяем ответ API на соответствие документации."""
     if not isinstance(response, dict):
         raise TypeError()
-    if not 'homeworks' in response:
-        raise KeyError('Не найден ключ homeworks.')
+    if 'homeworks' not in response:
+        raise KeyError()
     if isinstance(response.get('homeworks'), list):
         return response.get('homeworks')
     else:
@@ -102,10 +102,10 @@ def parse_status(homework):
     """Проверка статуса домашней работы."""
     if not isinstance(homework, dict):
         raise KeyError()
-    if 'status' in homework:
-        raise KeyError('В ответе нет ключа status.')
-    if 'homework_name' in homework:
-        raise KeyError('В ответе нет ключа homework_name.')
+    if 'status' not in homework:
+        raise KeyError()
+    if 'homework_name' not in homework:
+        raise KeyError()
     if not isinstance(homework.get('status'), str):
         raise TypeError()
     homework_name = homework.get('homework_name')
